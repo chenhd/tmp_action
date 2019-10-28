aeb767018010af4794904410b29b503d64253967
chenhd <chenhanduo.gray@gmail.com>
2019-10-28 13:23:24 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,1 +1,23 @@
 print("hello world")
+
+
+
+
+
+# content of test_sample.py
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
+
+
+def T():
+  assert True
+
+# def F():
+#   assert False
+
+def tmp():
+  assert False
