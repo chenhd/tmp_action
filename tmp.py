e1497849789c5aa86f111812990cccadb04a51dc
chenhd <chenhanduo.gray@gmail.com>
2019-10-28 13:43:51 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,17 +1,21 @@
-# def pytest_report_header(config):
-#     return "project deps: mylib-1.1"
+def pytest_report_header(config):
+    return "project deps: mylib-1.1"
 
-# content of test_some_are_slow.py
-import time
-
-
-def test_funcfast():
-    time.sleep(0.1)
+# # content of test_some_are_slow.py
+# import time
 
 
-def test_funcslow1():
-    time.sleep(0.2)
+# def test_funcfast():
+#     time.sleep(0.1)
 
 
-def test_funcslow2():
-    time.sleep(0.3)
+# def test_funcslow1():
+#     time.sleep(0.2)
+
+
+# def test_funcslow2():
+#     time.sleep(0.3)
+
+
+def test_a():
+    pass
